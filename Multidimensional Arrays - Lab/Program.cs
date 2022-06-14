7e9e6cc80703bbc41e8c25c3971255cc165bf0ed
Maya Ruseva <90904166+Kofka79@users.noreply.github.com>
2022-06-14 17:08:30 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Lab/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(", ")
+                                .Select(int.Parse).ToArray();
+            int rowsCount = dimensions[0];
+            int colsCount = dimensions[1];
+            int[,] matrix = new int[rowsCount, colsCount];
+
+            for (int row = 0; row < rowsCount; row++)
+            {
+                int[] line = Console.ReadLine().Split(", ")
+                              .Select(int.Parse).ToArray();
+                for (int col = 0; col < colsCount; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+            int sum = 0;
+            for (int row = 0; row < rowsCount; row++)
+            {
+                for (int col = 0; col < colsCount; col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+            Console.WriteLine(rowsCount);
+            Console.WriteLine(colsCount);
+            Console.WriteLine(sum);
+        }
+    }
+}
