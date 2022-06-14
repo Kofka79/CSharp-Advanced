537e42d2c2c57309f6d9d6eb24517006c265114e
Maya Ruseva <90904166+Kofka79@users.noreply.github.com>
2022-06-14 17:13:00 +0300

    Add files via upload

--- Multidimensional Arrays - Exercise/Program.cs
+++ Multidimensional Arrays - Exercise/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace _3._Maximal_Sum
+namespace _4._Matrix_Shuffling
 {
     class Program
     {
@@ -11,39 +11,83 @@ namespace _3._Maximal_Sum
                                 .Select(int.Parse).ToArray();
             int rowsCount = dimensions[0];
             int colsCount = dimensions[1];
-            int[,] matrix = new int[rowsCount, colsCount];
-            int rowMax = 0;
-            int colMax = 0;
+            string[,] matrix = new string[rowsCount, colsCount];
 
             for (int row = 0; row < rowsCount; row++)
             {
                 string[] line = Console.ReadLine().Split();
                 for (int col = 0; col < colsCount; col++)
                 {
-                    matrix[row, col] = int.Parse(line[col]);
+                    matrix[row, col] = line[col];
                 }
             }
-            int sumMax = int.MinValue;
-            for (int row = 0; row < rowsCount - 2; row++)
-            {
-                for (int col = 0; col < colsCount - 2; col++)
+            string cmd = Console.ReadLine();
+            while (cmd!="END")
+	        {
+
+                if (!CheckIsValid(cmd, rowsCount, colsCount))
                 {
-                    int currSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
-                       + matrix[row + 1, col] + matrix[row + 1, col + 1] +
-                       matrix[row + 1, col + 2] + matrix[row + 2, col] +
-                       matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
-                    if (currSum>sumMax)
+                    Console.WriteLine($"Invalid input!");
+                    cmd = Console.ReadLine();
+                    continue;
+                }
+                else
+                {
+                    string[] command = cmd.Split();
+                    int row1 = int.Parse(command[1]);
+                    int col1 = int.Parse(command[2]);
+                    int row2 = int.Parse(command[3]);
+                    int col2 = int.Parse(command[4]);
+                    string firstElement = matrix[row1, col1];
+                    string secondElement = matrix[row2, col2];
+                    matrix[row2, col2] = firstElement;
+                    matrix[row1, col1] = secondElement;
+
+
+                    for (int row = 0; row < rowsCount; row++)
                     {
-                        sumMax = currSum;
-                        rowMax = row;
-                        colMax = col;
+                        for (int col = 0; col < colsCount; col++)
+                        {
+                            Console.Write(matrix[row, col] + " ");
+                        }
+                        Console.WriteLine();
                     }
                 }
+                    
+                cmd = Console.ReadLine();
+	        }
+
+            
+        }
+        
+
+        private static bool CheckIsValid(string cmd, int rowsCount, int colsCount)
+        {
+            string[] command = cmd.Split();
+            string action = command[0];
+            
+            if (action == "swap" && command.Length == 5)
+            {
+                int row1 =int.Parse(command[1]);
+                int col1 =int.Parse(command[2]);
+                int row2 =int.Parse(command[3]);
+                int col2 =int.Parse(command[4]);
+                if (row1 >= 0 && row1 < rowsCount && row2 >= 0 && row2 < rowsCount &&
+                    col1 >= 0 && col1 < colsCount && col2 >= 0 && col2 < colsCount)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
-            Console.WriteLine($"Sum = {sumMax}");
-            Console.WriteLine(matrix[rowMax, colMax] + " " + matrix[rowMax, colMax + 1] + " " +matrix[rowMax,colMax+2]);
-            Console.WriteLine(matrix[rowMax+1, colMax] + " " + matrix[rowMax+1, colMax + 1] + " " + matrix[rowMax+1, colMax + 2]);
-            Console.WriteLine(matrix[rowMax+2, colMax] + " " + matrix[rowMax+2, colMax + 1] + " " + matrix[rowMax+2, colMax + 2]);
+            else
+            {
+                return false;
+            }
+                
+            
         }
     }
 }
