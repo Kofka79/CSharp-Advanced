c03b86093153f372179299c182b95767537bd4b2
Maya Ruseva <90904166+Kofka79@users.noreply.github.com>
2022-06-14 17:10:41 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] line = Console.ReadLine().Split(" ")
+                              .Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+            int sum1 = 0;
+            int sum2 = 0;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col)
+                    {
+                        sum1 += matrix[row, col];
+                    }
+                    if (row+col==n-1)
+                    {
+                        sum2 += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(sum1-sum2));
+        }
+    }
+}
