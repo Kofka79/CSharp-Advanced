0324006266e1f6a09a312c04afb0f6fefbd8d268
Maya Ruseva <90904166+Kofka79@users.noreply.github.com>
2022-06-14 17:06:06 +0300

    Add files via upload

---
+++ Stacks and Queues - Exercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputClothes =Console.ReadLine().Split().Select(int
+                .Parse).ToList();
+            Stack<int> clothes = new Stack<int>(inputClothes);
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int counter = 1;
+            int sum = 0;
+            
+            while (clothes.Any())
+            {
+                if (sum+clothes.Peek()<=rackCapacity)
+                {
+                sum += clothes.Pop();
+
+                }
+                else
+                {
+                    sum = 0;
+                    counter++;
+
+                }
+            }
+
+            
+            Console.WriteLine(counter);
+        }
+    }
+}
