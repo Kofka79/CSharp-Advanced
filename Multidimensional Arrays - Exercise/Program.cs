39521892f8fa161f4a2c4fbb93edaf3189e02fe9
Maya Ruseva <90904166+Kofka79@users.noreply.github.com>
2022-06-14 17:12:23 +0300

    Add files via upload

--- Multidimensional Arrays - Exercise/Program.cs
+++ Multidimensional Arrays - Exercise/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace _2._Squares_in_Matrix
+namespace _3._Maximal_Sum
 {
     class Program
     {
@@ -11,29 +11,39 @@ namespace _2._Squares_in_Matrix
                                 .Select(int.Parse).ToArray();
             int rowsCount = dimensions[0];
             int colsCount = dimensions[1];
-            string[,] matrix = new string[rowsCount, colsCount];
+            int[,] matrix = new int[rowsCount, colsCount];
+            int rowMax = 0;
+            int colMax = 0;
 
             for (int row = 0; row < rowsCount; row++)
             {
                 string[] line = Console.ReadLine().Split();
                 for (int col = 0; col < colsCount; col++)
                 {
-                    matrix[row, col] = line[col];
+                    matrix[row, col] = int.Parse(line[col]);
                 }
             }
-            int count = 0;
-            for (int row = 0; row < rowsCount-1; row++)
+            int sumMax = int.MinValue;
+            for (int row = 0; row < rowsCount - 2; row++)
             {
-                for (int col = 0; col < colsCount-1; col++)
+                for (int col = 0; col < colsCount - 2; col++)
                 {
-                    if (matrix[row,col]==matrix[row,col+1]&& matrix[row, col] == matrix[row+1, col]
-                       && matrix[row, col]==matrix[row+1,col+1] )
+                    int currSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
+                       + matrix[row + 1, col] + matrix[row + 1, col + 1] +
+                       matrix[row + 1, col + 2] + matrix[row + 2, col] +
+                       matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    if (currSum>sumMax)
                     {
-                        count++;
+                        sumMax = currSum;
+                        rowMax = row;
+                        colMax = col;
                     }
                 }
             }
-            Console.WriteLine(count);
+            Console.WriteLine($"Sum = {sumMax}");
+            Console.WriteLine(matrix[rowMax, colMax] + " " + matrix[rowMax, colMax + 1] + " " +matrix[rowMax,colMax+2]);
+            Console.WriteLine(matrix[rowMax+1, colMax] + " " + matrix[rowMax+1, colMax + 1] + " " + matrix[rowMax+1, colMax + 2]);
+            Console.WriteLine(matrix[rowMax+2, colMax] + " " + matrix[rowMax+2, colMax + 1] + " " + matrix[rowMax+2, colMax + 2]);
         }
     }
 }
