a4a65fa56ee9f491b544fa7ee448b0254d208236
Maya Ruseva <90904166+Kofka79@users.noreply.github.com>
2022-06-16 22:33:39 +0300

    Add files via upload

--- Exam Preparation/Exam-14 April 2021/Program.cs
+++ Exam Preparation/Exam-14 April 2021/Program.cs
@@ -1,51 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace _01.WarmWinter
+namespace _02._SuperMario
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] hats = Console.ReadLine().Split()
-                        .Select(int.Parse).ToArray();
-            int[] scarfs = Console.ReadLine().Split()
-                        .Select(int.Parse).ToArray();
-            Stack<int> hatStack = new Stack<int>(hats);
-            Queue<int> scarfQueue = new Queue<int>(scarfs);
-            Queue<int> sets = new Queue<int>();
-            int maxSet = 0;
-            while (hatStack.Count > 0 && scarfQueue.Count > 0)
+            int lives = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            char[][] field = new char[rows][];
+            for (int i = 0; i < rows; i++)
             {
-                int currHat = hatStack.Peek();
-                int currScarf = scarfQueue.Peek();
-                if (currHat>currScarf)
+                char[] chars = Console.ReadLine().ToCharArray();
+                    field[i]= chars;
+            }
+            int marioRow = 0;
+            int marioCol = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < field[i].Length; j++)
                 {
-                    int currSet = currScarf + currHat;
-                    if (currSet>maxSet)
+                    if (field[i][j]=='M')
                     {
-                        maxSet = currSet;
+                        marioRow = i;
+                        marioCol = j;
                     }
-                    hatStack.Pop();
-                    scarfQueue.Dequeue();
-                    sets.Enqueue(currSet);
-                            
-                }
-                else if (currHat<currScarf)
-                {
-                    hatStack.Pop();
-                }
-                else if (currHat==currScarf)
-                {
-                    scarfQueue.Dequeue();
-                    currHat++;
-                    hatStack.Pop();
-                    hatStack.Push(currHat);
                 }
             }
-            Console.WriteLine($"The most expensive set is: {maxSet}");
-            Console.WriteLine(string.Join(" ", sets));
+
+            while (true)
+            {
+                string commandLine = Console.ReadLine();
+                string[] commandParts = commandLine.Split(' ');
+                string command = commandParts[0];
+                int enemyRow =int.Parse(commandParts[1]);
+                int enemyCol =int.Parse(commandParts[2]);
+                
+                lives--;
+                field[marioRow][marioCol] = '-';
+                field[enemyRow][ enemyCol] = 'B';
+                //up
+                if (command=="W"&&marioRow-1>=0)
+                {
+                    marioRow--;
+                }
+                //down
+                else if (command=="S"&& marioRow+1<rows)
+                {
+                    marioRow++;
+                }
+                //left
+                else if (command=="A"&&marioCol-1>=0)
+                {
+                    marioCol--;
+                }
+                //right
+                else if (command=="D"&&marioCol+1<field[marioRow].Length)
+                {
+                    marioCol++;
+                }
+
+                if (field[marioRow][ marioCol]=='B')
+                {
+                    lives -= 2;
+                }
+                if (field[marioRow][marioCol]=='P')
+                {
+                    Console.WriteLine($"Mario has successfully saved the princess! Lives left: {lives}");
+                    field[marioRow][marioCol] = '-';
+                    break;
+                }
+
+                if (lives<=0)
+                {
+                    field[marioRow][marioCol] = 'X';
+                    Console.WriteLine($"Mario died at {marioRow};{marioCol}.");
+                    break;
+                }
+
+                field[marioRow][marioCol] = 'M';
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    Console.Write(field[i][j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
