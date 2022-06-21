566230dc9371d9c78cad34ab897dac8664f85e0d
Maya Ruseva <90904166+Kofka79@users.noreply.github.com>
2022-06-21 22:44:52 +0300

    Add files via upload

---
+++ Exam Preparation/Exam - 19 August 2020/03. VetClinic/Clinic.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        public Clinic(int capacity)
+        {
+            Pets = new List<Pet>();
+            Capacity = capacity;
+        }
+        
+        public List<Pet> Pets { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get{ return Pets.Count(); }
+        }
+
+        public void Add(Pet pet)
+        {
+            if (Pets.Count<Capacity)
+            {
+                Pets.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet currPet = Pets.FirstOrDefault(p => p.Name == name);
+            if (Pets.Contains(currPet))
+            {
+                Pets.Remove(currPet);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet newPet = Pets.FirstOrDefault(p => p.Name == name &&
+                          p.Owner == owner);
+            return newPet;
+        }
+
+        public Pet GetOldestPet()
+        {
+            return Pets.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+
+            foreach (var pet in Pets)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
