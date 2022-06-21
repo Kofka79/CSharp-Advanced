fcb39817c5af9072488dd3e858aa8c96ef1ea313
Maya Ruseva <90904166+Kofka79@users.noreply.github.com>
2022-06-21 21:22:39 +0300

    Add files via upload

---
+++ Exam Preparation/Exam - 20 February 2021/03. The Race/Race.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    class Race
+    {
+        public Race(string name, int capacity)
+        {
+            Racers = new List<Racer>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public List<Racer> Racers { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => Racers.Count;
+
+        public void Add(Racer racer)
+        {
+            if (Racers.Count<Capacity)
+            {
+                Racers.Add(racer);
+            }
+        }
+        public bool Remove(string name)
+        {
+            Racer currRacer = Racers.FirstOrDefault(r => r.Name == name);
+            if (Racers.Contains(currRacer))
+            {
+                Racers.Remove(currRacer);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public  Racer GetOldestRacer()
+        {
+            return Racers.OrderByDescending(r => r.Age).FirstOrDefault();
+        }
+
+        public Racer GetRacer(string name)
+        {
+            return Racers.FirstOrDefault(r => r.Name == name);
+        }
+
+        public Racer GetFastestRacer()
+        {
+            return Racers.OrderByDescending(c => c.Car.Speed).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Racers participating at {Name}:");
+            foreach (var racer in Racers)
+            {
+                sb.AppendLine(racer.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
